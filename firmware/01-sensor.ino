4a4e342b5e40badfe0672457289b50dd42372867
Sayanee <sayanee@gmail.com>
2017-08-26 15:21:48 +0800

    Add hardware connections in firmware notes

--- firmware/01-sensor.ino
+++ firmware/01-sensor.ino
@@ -1,11 +1,17 @@
 // INFO: Display humidity and temperature every second
+
 // Hardware: nodeMCU with Si7021 Adafruit breakout board
-// Arduino IDE Tools settings:
-// Board: NodeMCU board ESP-12E
-// Flash size: 4M
-// CPU Frequency: 80MHz
-// Upload speed: 115200
-// Port: /dev/cu.wchusbserial1410
+// - `VIN` on si7021 and `3V3` on nodeMCU
+// - `GND` on si7021 and `G` on nodeMCU
+// - `SDA` on si7021 and `D3` on nodeMCU
+// - `SCL` on si7021 and `D4` on nodeMCU
+
+// IDE: Arduino Tools settings
+// - Board: NodeMCU board ESP-12E
+// - Flash size: 4M
+// - CPU Frequency: 80MHz
+// - Upload speed: 115200
+// - Port: /dev/cu.wchusbserial1410
 
 #include <SI7021.h>
 
