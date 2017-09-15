9170e7600cce436991de1a2ccb1d3f6b4051b577
Sayanee <sayanee@gmail.com>
2017-09-15 21:33:29 +0800

    Act on something every 3 hours after counting every 45 minutes

--- firmware/05-sleep-long.ino
+++ firmware/05-sleep-long.ino
@@ -17,9 +17,9 @@ extern "C" {
   #include "user_interface.h"
 }
 
-const int sleepTime = 1800; // 1800 seconds = 30 minutes
+const int sleepTime = 2700; // 2700 seconds = 45 minutes
 
-#define FINAL_SLEEP_INTERVAL 4 // 4 * sleepTime = 2 hours
+#define FINAL_SLEEP_INTERVAL 4 // 4 * sleepTime = 3 hours
 #define CURRENT_SLEEP_INTERVAL_ADDR 30 // EEPROM address to store sleep interval
 #define CURRENT_SLEEP_INTERVAL EEPROM.read(CURRENT_SLEEP_INTERVAL_ADDR)
 
@@ -36,7 +36,7 @@ void setup() {
   if (CURRENT_SLEEP_INTERVAL == FINAL_SLEEP_INTERVAL) {
     blink(5);
     resetSleepInterval();
-    doSomething(); // do something after 2 hours only
+    doSomething(); // do something after 3 hours only
     goToSleep();
   } else {
     blink(5);
@@ -61,22 +61,15 @@ void doSomething() {
 }
 
 void goToSleep() {
-  Serial.println("[INFO] Going to sleep ...");
   ESP.deepSleep(sleepTime * 1000000);
 }
 
 void resetSleepInterval() {
   EEPROM.write(CURRENT_SLEEP_INTERVAL_ADDR, 0);
   EEPROM.commit();
-
-  Serial.print("[INFO] Reset sleep interval: ");
-  Serial.println(CURRENT_SLEEP_INTERVAL);
 }
 
 void increaseSleepInterval() {
   EEPROM.write(CURRENT_SLEEP_INTERVAL_ADDR, CURRENT_SLEEP_INTERVAL + 1);
   EEPROM.commit();
-
-  Serial.print("[INFO] Increase sleep interval: ");
-  Serial.println(CURRENT_SLEEP_INTERVAL);
 }
