321fae618b6e9aeb1360d86460b12a58e31b0fbc
Sayanee <sayanee@gmail.com>
2017-09-25 07:12:05 +0800

    Add circuit for button press and sleep interval

--- firmware/05-sleep-long-or-button-press/firmware.ino
+++ firmware/05-sleep-long-or-button-press/firmware.ino
@@ -1,8 +1,6 @@
 // INFO: Wake up after every long sleep interval or button press
 
-// Hardware: nodeMCU to button for interrupt
-// Short pin D0 to pin RST
-// Note: Un-short the 2 pins above during uploading firmware
+// Hardware: See schematic.png
 
 #include <EEPROM.h>
 
@@ -16,14 +14,15 @@ const int sleepTime = 10; // 10 seconds
 #define CURRENT_SLEEP_INTERVAL_ADDR 30 // EEPROM address to store sleep interval
 #define CURRENT_SLEEP_INTERVAL EEPROM.read(CURRENT_SLEEP_INTERVAL_ADDR)
 #define BUTTON_PRESS 12 // GPIO12 or D6
-
-int latchPin = 15; // pin D8 on NodeMCU boards
-int clockPin = 14; // pin D5 on NodeMCU boards
-int dataPin = 13; // pin D7 on NodeMCU boards
+int val = 0;
 
 void setup() {
-  EEPROM.begin(512);
+  val = digitalRead(BUTTON_PRESS);
+
   Serial.begin(115200);
+  Serial.println(val);
+
+  EEPROM.begin(512);
   pinMode(2, OUTPUT);
   Serial.println("[INFO] Wake up!");
 
@@ -50,6 +49,10 @@ void readPin() {
 }
 
 void blink(int times) {
+  Serial.print("[INFO] Blink for ");
+  Serial.print(times);
+  Serial.println(" times");
+
   for (int i=0; i <= times; i++){
     digitalWrite(2, HIGH);
     delay(1000);
@@ -76,6 +79,9 @@ void resetSleepInterval() {
 }
 
 void increaseSleepInterval() {
+  Serial.print("[INFO] Current sleep interval: ");
+  Serial.println(CURRENT_SLEEP_INTERVAL);
+
   EEPROM.write(CURRENT_SLEEP_INTERVAL_ADDR, CURRENT_SLEEP_INTERVAL + 1);
   EEPROM.commit();
 }
