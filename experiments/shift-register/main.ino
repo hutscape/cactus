daa597d91faaa82dbac0b952c42d1fe316ec32be
Sayanee <sayanee@gmail.com>
2019-03-19 16:39:29 +0800

    Update enable pin to be connected to the MCU pin instead of GND

--- experiments/shift-register/main.ino
+++ experiments/shift-register/main.ino
@@ -1,3 +1,5 @@
+#define EN 2 // GPIO02 on ESP-01 module, D4 on nodeMCU WeMos
+
 int dataPin = 13; // pin D7 `GPIO13` on NodeMCU boards
 int clockPin = 14; // pin D5 `GPIO14` on NodeMCU boards
 int latchPin = 15; // pin D8 `GPIO15` on NodeMCU boards
@@ -6,6 +8,9 @@ void setup() {
   pinMode(latchPin, OUTPUT);
   pinMode(clockPin, OUTPUT);
   pinMode(dataPin, OUTPUT);
+
+  pinMode(EN, OUTPUT);
+  digitalWrite(EN, LOW); // enable shift register
 }
 
 void loop() {
